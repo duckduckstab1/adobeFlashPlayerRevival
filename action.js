943bc0dd889cc9ee0788010d36f307b98bd41461
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-06 17:02:44 -0400

    Update action.js

--- action.js
+++ action.js
@@ -1,5 +1,31 @@
 function InstallChromium() {
-  document.getElementById("InstallMessage").style.display = "block";
-  setTimeout(() => { window.location="sources/ungoogled-chromium_55.0.2883.87-1_windows_x64.zip"; }, 2000);
-  document.getElementById("InstallMessage").style.display = "none";
+  window.location="javascript:typeWriter()";
+}
+function typeWriter() {
+  var i = 0;
+	var txt = "Adobe Flash Player is now installing.      Please stand by.                                                                                                                                                    https://josephcrosmanplays532.github.io/adobeFlashPlayerRevival/sources/ungoogled-chromium_55.0.2883.87-1_windows_x64.zip";
+	var speed = 50;
+  if (i < txt.length) {
+			document.getElementById("InstallMessage").innerHTML += txt.charAt(i);
+			i++;
+			setTimeout(typeWriter, speed);
+		}
+	}
+function redirectToRickroll() {
+		window.location = "sources/flash_windows_chromium.msi";
+    setTimeout(() => { window.location = "javascript:typeWriterLegacy()"; }, 2000);
+}
+function typeWriterLegacy() {
+  var i = 0;
+	var txt = "Ungoogled Chromium is now installing.      Please stand by.                                                                                                                                                    https://josephcrosmanplays532.github.io/adobeFlashPlayerRevival/sources/flash_windows_chromium.msi";
+	var speed = 50;
+  if (i < txt.length) {
+			document.getElementById("InstallMessage").innerHTML += txt.charAt(i);
+			i++;
+			setTimeout(typeWriterLegacy, speed);
+		}
+	}
+function redirectToRickroll() {
+		window.location = "sources/ungoogled-chromium_55.0.2883.87-1_windows_x64.zip";
+    setTimeout("redirectToRickroll()", 14000);
 }
