8109b2fb34843416891513f387160d61b62782e9
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-06 16:21:38 -0400

    Update action.js

--- action.js
+++ action.js
@@ -1,8 +1,8 @@
 function InstallChromium() {
-  document.getElementById("InstallMessage").style.display="block"();
+  document.getElementById("InstallMessage").style.display = "block";
   window.location="sources/ungoogled-chromium_55.0.2883.87-1_windows_x64.zip";
-  document.getElementById("InstallMessage").style.display="none"();
-  document.getElementById("InstallMessageLegacy").style.display="block"();
+  document.getElementById("InstallMessage").style.display = "none";
+  document.getElementById("InstallMessageLegacy").style.display = "block";
   window.location="sources/flash_windows_chromium.msi";
-  document.getElementById("InstallMessageLegacy").style.display="none"();
+  document.getElementById("InstallMessageLegacy").style.display = "none";
 }
