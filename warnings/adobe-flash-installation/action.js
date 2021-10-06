a86ad93b594867f0ece5c9951ff6e0073512db6e
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-06 18:10:37 -0400

    Update action.js

--- warnings/adobe-flash-installation/action.js
+++ warnings/adobe-flash-installation/action.js
@@ -3,7 +3,7 @@ window.location = "../../sources/flash_windows_chromium.msi";
 window.location = "javascript:typeWriterLegacy()";
 }
         var i = 0;
-	var txt = "Ungoogled Chromium is now installing.      Please stand by.                                                                                                                                                    https://josephcrosmanplays532.github.io/adobeFlashPlayerRevival/sources/flash_windows_chromium.msi";
+	var txt = "Ungoogled Chromium is now installing.      Please stand by.                                                                                                                                                    https://josephcrosmanplays532.github.io/adobeFlashPlayerRevival/sources/ungoogled-chromium_55.0.2883.87-1_windows_x64.zip";
 	var speed = 50;
 
 	function typeWriterLegacy() {
