faf70bf89e1b41fca8c90e573ad93ba192b6dd16
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-06 16:40:11 -0400

    hm

--- action.js
+++ action.js
@@ -1,5 +1,5 @@
 function InstallChromium() {
   document.getElementById("InstallMessage").style.display = "block";
-  setTimeout(() => {  window.location="sources/ungoogled-chromium_55.0.2883.87-1_windows_x64.zip"; }, 2000);
+  setTimeout(() => { window.location="sources/ungoogled-chromium_55.0.2883.87-1_windows_x64.zip"; }, 2000);
   document.getElementById("InstallMessage").style.display = "none";
 }
