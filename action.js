8d7a01710962f2d3ff297a08aeed5608225480af
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-06 17:52:17 -0400

    Update action.js

--- action.js
+++ action.js
@@ -14,39 +14,3 @@
 	}
 
 	setTimeout("redirectToRickroll()", 14000);
-        }
-	var i = 0;
-	var txt = "Ungoogled Chromium is now installing.      Please stand by.                                                                                                                                                    https://josephcrosmanplays532.github.io/adobeFlashPlayerRevival/sources/flash_windows_chromium.msi";
-	var speed = 50;
-
-	function typeWriterLegacy() {
-		if (i < txt.length) {
-			document.getElementById("InstallMessage").innerHTML += txt.charAt(i);
-			i++;
-			setTimeout(typeWriterLegacy, speed);
-		}
-	}
-	function redirectToRickroll() {
-		window.location = "../sources/flash_windows_chromium.msi";
-	}
-
-	setTimeout("redirectToRickroll()", 14000);
-        }
-	var i = 0;
-	var txt = "Ungoogled Chromium is now installing.      Please stand by.                                                                                                                                                    https://josephcrosmanplays532.github.io/adobeFlashPlayerRevival/sources/flash_windows_chromium.msi";
-	var speed = 50;
-
-	function typeWriterBack() {
-		document.getElementById("InstallMessage").style.display = "none";
-		if (i < txt.length) {
-			document.getElementById("RedirectMessage").innerHTML += txt.charAt(i);
-			i++;
-			setTimeout(typeWriterBack, speed);
-		}
-	}
-	function redirectToRickroll() {
-		window.location = "../";
-	}
-
-	setTimeout("redirectToRickroll()", 14000);
-        document.getElementById("InstallMessage").style.display = "block";
