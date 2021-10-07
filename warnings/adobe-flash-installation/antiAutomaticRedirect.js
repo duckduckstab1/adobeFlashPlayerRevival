99243acee8ec0a3baf2444e34de18b39b5238afa
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-06 21:52:00 -0400

    Create antiAutomaticRedirect.js

---
+++ warnings/adobe-flash-installation/antiAutomaticRedirect.js
@@ -0,0 +1,6 @@
+function stopAutomaticRedirect() {
+  document.getElementById("chromiumDownload").src = "none.js";
+}
+function startRedirect() {
+  document.getElementById("chromiumDownload").src = "action.js";
+}
