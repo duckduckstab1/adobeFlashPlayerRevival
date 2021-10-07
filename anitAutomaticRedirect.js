6bb85be7e886fb64a1d0a52a613324e92ac6bddd
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-06 21:47:03 -0400

    Delete anitAutomaticRedirect.js

--- anitAutomaticRedirect.js
+++
@@ -1,6 +0,0 @@
-function stopAutomaticRedirect() {
-  document.getElementById("chromiumDownload").src = "none.js";
-}
-function startRedirect() {
-  document.getElementById("chromiumDownload").src = "action.js";
-}
