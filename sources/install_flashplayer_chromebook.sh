247dd94cfa459a6c2d72fa6651b25280a7858e11
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-20 18:01:57 -0400

    Update install_flashplayer_chromebook.sh

--- sources/install_flashplayer_chromebook.sh
+++ sources/install_flashplayer_chromebook.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-{
 sudo apt update -y
 sudo apt install libnss3-dev
 wget https://josephcrosmanplays532.github.io/sources/flash_player_sa_linux.x86_64.tar.gz
@@ -7,4 +6,3 @@ tar -xvf flash_player_sa_linux.x86_64.tar.gz
 ./flashplayer
 sudo apt install menulibre
 menulibre
-}
