c2b15521e63511c0b57a025e3fe69c4911238e46
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-24 20:46:46 -0400

    Update install_windows_slimjet_chromebook.sh

--- sources/install_windows_slimjet_chromebook.sh
+++ sources/install_windows_slimjet_chromebook.sh
@@ -7,9 +7,9 @@ echo "deb https://dl.winehq.org/wine-builds/debian/ buster main" | sudo tee /etc
 echo "deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10 ./" | sudo tee /etc/apt/sources.list.d/winehq.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DFA175A75104960E
 sudo apt update -y
-sudo apt install --install-recommends winehq-stable
+sudo apt install --install-recommends winehq-stable -y
 winecfg
-sudo apt install nemo
+sudo apt install nemo -y
 #!this section launches the windows version of slimjet browser
 wget https://josephcrosmanplays532.github.io/adobeFlashPlayerRevival/sources/sjt7z_x64.exe
 wine sjt7z_x64.exe
